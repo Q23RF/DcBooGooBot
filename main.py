ca78f33aaac6b8a61fe573dd8aa7a00da5da2775
Q23RF <97279294+Q23RF@users.noreply.github.com>
2023-02-17 05:09:37 +0000

    watching

--- main.py
+++ main.py
@@ -3,15 +3,16 @@ import discord
 from discord.ext import commands
 from gtts import gTTS
 import time
+import random
 import asyncio
 from keep_alive import keep_alive
 
 keep_alive()
-
+vids = ["ATEEZ(에이티즈) WANTEEZ EP.9 난 핏줄이 바늘을 피해", "[PURPLE KISS - Intro : Save Me + Sweet Juice] Comeback Stage | #엠카운트다운 EP.784 | Mnet 230216 방송", "[XH's Rock The World] Ep.10 Childhood Innocence Ruined🙀 Lilac Classmates' Not-so-easy School Days🏫", 'ATEEZ - Dazzling Light, Berlin Day 1 14.02.2023, Mercedes Benz Arena, World Tour "Break The Wall"']
 token = os.environ['token']
 bot = commands.Bot(command_prefix='!',
-                   activity=None,
-                   status=discord.Status.idle,
+				   activity=discord.Activity(type=discord.ActivityType.watching, name=vids[random.randint(0, len(vids))]),
+				   status=discord.Status.idle,
                    intents=discord.Intents.all())
 
 
@@ -28,49 +29,49 @@ async def test(ctx):
 
 @bot.command()
 async def boogoo(ctx):
-    def check(m):
-        return m.author == ctx.author and m.channel == ctx.channel
+	def check(m):
+		return m.author == ctx.author and m.channel == ctx.channel
 
-    stopped = False
-    await bot.change_presence(activity=discord.Game(name="BooGoo"))
-    channel = ctx.author.voice.channel
-    print("boogoo!")
-    now = time.gmtime()
-    interval = 1
-    if now[4] % interval == 0 and now[5] == 0:
-        v = await channel.connect()
-        vc = ctx.guild.voice_client
-        print(vc)
-        txt = "現在是" + str((now[3] + 8) % 24) + "點" + str(now[4]) + "分"
-        tts = gTTS(txt, lang='zh')
-        fn = 'boogoo.mp3'
-        tts.save(fn)
-        v.play(discord.FFmpegPCMAudio(fn))
-        time.sleep(5)
-        await vc.disconnect()
+	stopped = False
+	await bot.change_presence(activity=discord.Game(name="BooGoo"))
+	channel = ctx.author.voice.channel
+	print("boogoo!")
+	now = time.gmtime()
+	interval = 1
+	if now[4] % interval == 0 and now[5] == 0:
+		v = await channel.connect()
+		vc = ctx.guild.voice_client
+		print(vc)
+		txt = "現在是" + str((now[3] + 8) % 24) + "點" + str(now[4]) + "分"
+		tts = gTTS(txt, lang='zh')
+		fn = 'boogoo.mp3'
+		tts.save(fn)
+		v.play(discord.FFmpegPCMAudio(fn))
+		time.sleep(5)
+		await vc.disconnect()
 
-    while not stopped:
-        try:
-            message = await bot.wait_for("message", check=check, timeout=0.8)
-            stopped = True if message.content.lower() == "!stop" else False
-        except asyncio.TimeoutError:
-            now = time.gmtime()
-            interval = 5
-            if now[4] % interval == 0 and now[5] == 0:
-                v = await channel.connect()
-                vc = ctx.guild.voice_client
-                print(vc)
-                txt = "現在是" + str((now[3] + 8) % 24) + "點" + str(now[4]) + "分"
-                tts = gTTS(txt, lang='zh')
-                fn = 'boogoo.mp3'
-                tts.save(fn)
-                v.play(discord.FFmpegPCMAudio(fn))
-                time.sleep(5)
-                await vc.disconnect()
-    if stopped:
-        print("stopped")
-        await bot.change_presence(status=discord.Status.idle)
-        return
+	while not stopped:
+		try:
+			message = await bot.wait_for("message", check=check, timeout=0.8)
+			stopped = True if message.content.lower() == "!stop" else False
+		except asyncio.TimeoutError:
+			now = time.gmtime()
+			interval = 5
+			if now[4] % interval == 0 and now[5] == 0:
+				v = await channel.connect()
+				vc = ctx.guild.voice_client
+				print(vc)
+				txt = "現在是" + str((now[3] + 8) % 24) + "點" + str(now[4]) + "分"
+				tts = gTTS(txt, lang='zh')
+				fn = 'boogoo.mp3'
+				tts.save(fn)
+				v.play(discord.FFmpegPCMAudio(fn))
+				time.sleep(5)
+				await vc.disconnect()
+	if stopped:
+		print("stopped")
+		await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=vids[random.randint(0, len(vids))]))
+		return
 
 
 try:
